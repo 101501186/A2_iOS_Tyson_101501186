2b48b4e08b6edb17eb2f31dd645fb502eeacc82c
101501186 <101501186@georgebrown.ca>
2026-03-31 17:42:38 -0500

    Product managed object classes

---
+++ A2_iOS_Tyson_101501186/Product+CoreDataProperties.swift
@@ -0,0 +1,18 @@
+import Foundation
+import CoreData
+
+extension Product {
+
+    @nonobjc class func fetchRequest() -> NSFetchRequest<Product> {
+        NSFetchRequest<Product>(entityName: "Product")
+    }
+
+    @NSManaged var productID: Int64
+    @NSManaged var name: String?
+    @NSManaged var productDescription: String?
+    @NSManaged var price: Double
+    @NSManaged var provider: String?
+}
+
+extension Product: Identifiable {
+}
