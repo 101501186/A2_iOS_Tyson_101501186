d245ada28d11f3a34eb18480fad0be8a063f62cc
101501186 <101501186@georgebrown.ca>
2026-03-31 14:00:21 -0500

    Initialized project

---
+++ A2_iOS_Tyson_101501186/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_iOS_Tyson_101501186
+//
+//  Created by Tyson Ward-Dicks on 2026-03-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
