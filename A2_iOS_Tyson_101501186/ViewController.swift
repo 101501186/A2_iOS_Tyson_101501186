c70bd610191025ad22a85b00e98938e38a47462d
101501186 <101501186@georgebrown.ca>
2026-04-01 21:10:06 -0500

    Displays first product on launch

--- A2_iOS_Tyson_101501186/ViewController.swift
+++ A2_iOS_Tyson_101501186/ViewController.swift
@@ -3,11 +3,30 @@ import UIKit
 class ViewController: UIViewController {
 
     let repository = ProductRepository()
+    var products: [Product] = []
+
+    @IBOutlet weak var productIDLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var providerLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         repository.seedProductsIfNeeded()
+        products = repository.fetchAllProducts()
+
+        if let firstProduct = products.first {
+            displayProduct(firstProduct)
+        }
+    }
+
+    func displayProduct(_ product: Product) {
+        productIDLabel.text = "Product ID: \(product.productID)"
+        nameLabel.text = "Name: \(product.name ?? "")"
+        descriptionLabel.text = "Description: \(product.productDescription ?? "")"
+        priceLabel.text = String(format: "Price: $%.2f", product.price)
+        providerLabel.text = "Provider: \(product.provider ?? "")"
     }
 }
-
 
